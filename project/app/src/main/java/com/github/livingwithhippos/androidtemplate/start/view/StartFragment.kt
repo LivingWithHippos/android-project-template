f5e059a44ffcf8d3ff6aec106ad7fb7b305298c7
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-06 17:15:48 +0200

    load github jobs in fragment
    only logged for now

--- project/app/src/main/java/com/github/livingwithhippos/androidtemplate/start/view/StartFragment.kt
+++ project/app/src/main/java/com/github/livingwithhippos/androidtemplate/start/view/StartFragment.kt
@@ -1,12 +1,13 @@
 package com.github.livingwithhippos.androidtemplate.start.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.github.livingwithhippos.androidtemplate.R
+import androidx.lifecycle.Observer
 import com.github.livingwithhippos.androidtemplate.databinding.FragmentStartBinding
 import com.github.livingwithhippos.androidtemplate.start.model.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,6 +28,14 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentStartBinding.inflate(inflater, container, false)
+
+        viewModel.positionsLiveData.observe(viewLifecycleOwner, Observer { positions ->
+            positions?.forEach { Log.d("StartFragment", "Position: $it") }
+        })
+
+        binding.bLoad.setOnClickListener {
+            viewModel.fetchPositionsInfo("android", "berlin")
+        }
         return binding.root
     }
 
