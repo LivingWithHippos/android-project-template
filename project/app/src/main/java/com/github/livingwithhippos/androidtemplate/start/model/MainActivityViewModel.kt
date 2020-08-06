3ccc0b89b0835f47704d309eebb7eb1aa575e209
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-06 17:15:24 +0200

    added fetchPositionsInfo to viewmodel

--- project/app/src/main/java/com/github/livingwithhippos/androidtemplate/start/model/MainActivityViewModel.kt
+++ project/app/src/main/java/com/github/livingwithhippos/androidtemplate/start/model/MainActivityViewModel.kt
@@ -2,10 +2,40 @@ package com.github.livingwithhippos.androidtemplate.start.model
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.github.livingwithhippos.androidtemplate.model.network.api.Position
+import com.github.livingwithhippos.androidtemplate.model.repository.PositionRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel @ViewModelInject constructor(
-    @Assisted private val savedStateHandle: SavedStateHandle
+    @Assisted private val savedStateHandle: SavedStateHandle,
+    private val positionRepository: PositionRepository
 ) : ViewModel() {
+
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
+
+    val positionsLiveData = MutableLiveData<List<Position>?>()
+
+    fun fetchPositionsInfo(
+        description: String,
+        location: String?,
+        lat: String? = null,
+        long: String? = null,
+        full_time: Boolean? = true,
+        page: Int? = 0
+    ) {
+        scope.launch {
+            val positions =
+                positionRepository.getPositions(description, location, lat, long, full_time, page)
+            positionsLiveData.postValue(positions)
+        }
+    }
+
+    fun cancelRequests() = job.cancel()
 }
